417ee454a3d533ab1320f397fe498faf96227e3a
kaz <kazan19066@gmail.com>
2024-10-21 20:47:45 +0400

    url changed

--- app/src/main/java/com/example/burgerapplication/api/AppModule.kt
+++ app/src/main/java/com/example/burgerapplication/api/AppModule.kt
@@ -16,7 +16,7 @@ object AppModule {
     @Singleton
     fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://burgerrestaurantbackend.onrender.com/")
+            .baseUrl("https://ashashkov.com/project/burgerRestaurant/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
