32527e0cefa7fa5cc362209311a0a939571b41e6
kaz <kazan19066@gmail.com>
2025-07-09 16:25:53 +0400

    fix test

--- app/src/test/java/com/example/burgerapplication/adapter/CartAdapterTest.kt
+++ app/src/test/java/com/example/burgerapplication/adapter/CartAdapterTest.kt
@@ -1,9 +1,12 @@
 package com.example.burgerapplication.adapter
 
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.lifecycle.LifecycleOwner
 import com.example.burgerapplication.dto.Cart
 import com.example.burgerapplication.dto.Product
 import com.example.burgerapplication.viewmodel.CartViewModel
+import com.facebook.shimmer.ShimmerFrameLayout
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -16,12 +19,15 @@ class CartAdapterTest {
     private val cartViewModel = mockk<CartViewModel>()
     private val cart: Cart = mockk(relaxed = true)
     private val items: List<Product> = listOf()
+    private val progressBar: ProgressBar = mockk(relaxed = true)
+    private val shimmerFinalPrice: ShimmerFrameLayout = mockk(relaxed = true)
+    private val finalPrice: TextView = mockk(relaxed = true)
 
     @Test
     fun calculateTotalPriceFor100(): Unit = runTest {
         val price = "100"
         val quantity = 2
-        val adapter = CartAdapter(items, cartViewModel, cart, lifecycleOwner, this)
+        val adapter = CartAdapter(items, cartViewModel, cart, lifecycleOwner, this, progressBar, shimmerFinalPrice, finalPrice)
         val result = adapter.calculateTotalPrice(price, quantity)
 
         assertEquals(200.0, result)
